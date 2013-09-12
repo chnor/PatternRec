861ed29baa0b3fc1e6bba782e2bd3a3df7b88c46
Christopher Norman <chnor@kth.se>
2013-09-12 11:11:15 +0200

    Cleaned up @DiscreteD rand

--- PattRecClasses/@DiscreteD/rand.m
+++ PattRecClasses/@DiscreteD/rand.m
@@ -17,20 +17,30 @@ if numel(pD)>1
     error('Method works only for a single DiscreteD object');
 end;
 
+% Theory:
+% For any distribution D with cumulative probability density
+% function cdf(x) we can draw random numbers from the distribution
+% by sampling a (pseudo)random number t uniformly from [0, 1]
+% and returning cdf_inv(t) [see e.g. Knuth vol 2].
+% In the case of discrete distributions this amount to finding
+% the first index where the cumulative probability exceeds t.
+
 % Solution 1: loop
-%R = zeros(nData, 1);
+R = zeros(nData, 1);
 % We can of course use parfor here since the loop has
 % perfect parallelism but this is probably not a major
 % bottleneck...
-%for i = 1:nData
-%    R(i) = find(rand() <= cumsum(double(pD)), 1, 'first');
-%end
+d = cumsum(double(pD));
+for i = 1:nData
+   R(i) = find(rand() <= d, 1, 'first');
+end
 
-% Solution 2: Matlab magic
+% Solution 2: The same as the loop except it uses matrix
+% operations.
 % res = bsxfun(@gt, cumsum(double(pD)), rand(nData, 1));
 % % Find first in each row
 % [R, ~] = find(res' == 1 & cumsum(res, 2)' == 1);
 
 % Solution 3: builtin
-mass = double(pD);
-R = randsample(1:length(mass), nData, 'true', mass);
+% mass = double(pD);
+% R = randsample(1:length(mass), nData, 'true', mass);
