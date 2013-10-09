bd2cf9cedaad3fd1a016d8420c5a45bf25ae79d3
Christopher Norman <chnor@kth.se>
2013-10-09 17:15:35 +0200

    Split userInput

---
+++ Assignment5/InputWithConfusion.m
@@ -0,0 +1,47 @@
+
+%Input:
+%   Classifier: stuct containing:
+%       - HMMs: a cell array of HMMs that recognize each character
+%       - classes: the class label for the corresponding HMM
+%       - features: used features
+%   characterToMatrix: the character you will display, so that the
+%                      Confusion Matrix will be displayed
+
+function InputWithConfusion(Classifier, characterToMatrix)
+
+HMMs        = Classifier.HMMs;
+classes     = Classifier.classes;
+features    = Classifier.features;
+
+character = DrawCharacter();
+character = ExtractFeatures(character);
+proba = logprob([HMMs{1,:}], character(features,:));
+[~, index] = max(proba);
+
+disp(['Written letter: ', classes(index)]);
+
+%Improvement: - allow the db length to vary.
+%             - Display the letters which are not in the diagonal and create
+%               confusion
+
+if nargin == 2
+    
+    savefile = 'confusionMatrix.mat';
+    finder = cell2mat(classes);
+    index2 = strfind(finder, characterToMatrix);
+    
+    if exist(savefile) == 2
+        data = load(savefile);
+        confusionMatrix = data.confusionMatrix;
+        confusionMatrix(index, index2) = confusionMatrix(index, index2) + 1;
+        
+    else
+        length = size (classes, 2);
+        confusionMatrix = zeros(length, length);
+        confusionMatrix (index, index2) = 1;
+        
+    end
+    save(savefile, 'confusionMatrix');
+end
+
+end
--- Assignment5/userInput.m
+++ Assignment5/userInput.m
@@ -1,46 +1,23 @@
-function userInput(Classifier, characterToMatrix)
-
-%Input:
+% Get input from user and return the most likely character
+% Input:
 %   Classifier: stuct containing:
 %       - HMMs: a cell array of HMMs that recognize each character
 %       - classes: the class label for the corresponding HMM
 %       - features: used features
-%   characterToMatrix: the character you will display, so that the
-%                      Confusion Matrix will be displayed
+% Output:
+%   char: The label of the decided character
 
-HMMs        = Classifier.HMMs;
-classes     = Classifier.classes;
-features    = Classifier.features;
-
-character = DrawCharacter();
-character = ExtractFeatures(character);
-proba = logprob([HMMs{1,:}], character(features,:));
-[~, index] = max(proba);
-
-disp(['Written letter: ', classes(index)]);
-
-%Improvement: - allow the db length to vary.
-%             - Display the letters which are not in the diagonal and create
-%               confusion
-
-if nargin == 2
+function char = userInput(Classifier)
     
-    savefile = 'confusionMatrix.mat';
-    finder = cell2mat(classes);
-    index2 = strfind(finder, characterToMatrix);
-    
-    if exist(savefile) == 2
-        data = load(savefile);
-        confusionMatrix = data.confusionMatrix;
-        confusionMatrix(index, index2) = confusionMatrix(index, index2) + 1;
-        
-    else
-        length = size (classes, 2);
-        confusionMatrix = zeros(length, length);
-        confusionMatrix (index, index2) = 1;
-        
-    end
-    save(savefile, 'confusionMatrix');
-end
+    HMMs        = Classifier.HMMs;
+    classes     = Classifier.classes;
+    features    = Classifier.features;
 
+    character = DrawCharacter();
+    character = ExtractFeatures(character);
+    proba = logprob([HMMs{1,:}], character(features,:));
+    [~, index] = max(proba);
+    
+    char = classes(index);
+    
 end
