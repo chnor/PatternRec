e45952edccfd4a5e328291063570825772bf0846
Christopher Norman <chnor@kth.se>
2013-10-10 16:11:03 +0200

    LoadDictionary + ANC corpus
    LoadDictionary loads word frequency corpus in the ANC format and
    calculates the log probability for each transition in the trie, as well
    as the ending log probability for each node.

--- PattRecClasses/AddWordToDictionary.m
+++ PattRecClasses/AddWordToDictionary.m
@@ -1,19 +1,23 @@
-function dict = AddWordToDictionary(word, dict)
+function dict = AddWordToDictionary(word, dict, n)
     
     assert(~isempty(word));
+    
+    if nargin < 3
+        n = 1;
+    end
     
     [t, nodes] = TrieLookup(word, dict.trie);
     dict.trie = t;
     if nodes(end) > length(dict.word_count)
         dict.word_count(nodes(end)) = 0;
     end
-    dict.word_count(nodes(end)) = dict.word_count(nodes(end)) + 1;
+    dict.word_count(nodes(end)) = dict.word_count(nodes(end)) + n;
     for node = nodes
         if node > length(dict.subword_count)
             dict.subword_count(node) = 0;
         end
-        dict.subword_count(node) = dict.subword_count(node) + 1;
+        dict.subword_count(node) = dict.subword_count(node) + n;
     end
-    dict.total_word_count = dict.total_word_count + 1;
+    dict.total_word_count = dict.total_word_count + n;
     
 end
---
+++ data/LoadDictionary.m
@@ -0,0 +1,35 @@
+function dictionary = LoadDictionary(corpusname, n_words_to_extract)
+    
+    dictionary = ConstructDictionary;
+    
+    corpus = fopen(corpusname);
+    entries = textscan(corpus, '%*s %s %*s %d\n');
+    words = entries{1}';
+    freqs = entries{2}';
+    n_words_to_extract = min(n_words_to_extract, length(freqs));
+    for i = 1:n_words_to_extract
+        disp(['Extracting: "', words{i}, '" with rank: ', num2str(i), ' frequency: ', num2str(freqs(i))]);
+        dictionary = AddWordToDictionary(words{i}, dictionary, freqs(i));
+    end
+    
+    % Calculate log probabilities for each node transition in tree
+    % and the log probabilities for ending in each node
+    
+    % Hackish: TODO do something better
+    dictionary.end_p = -Inf*ones(size(dictionary.word_count));
+    dictionary.transition_p = -Inf*ones(size(dictionary.subword_count));
+    
+    for node = dictionary.trie.nodeorderiterator
+        subnodes = getchildren(dictionary.trie, node);
+        total_count = sum(dictionary.subword_count(subnodes));
+        total_count = total_count + dictionary.word_count(node);
+        if total_count == 0
+            continue;
+        end
+        dictionary.transition_p(subnodes) = log(dictionary.subword_count(subnodes)) - log(total_count);
+        if dictionary.word_count(node) > 0
+            dictionary.end_p(node) = log(dictionary.word_count(node)) - log(total_count);
+        end
+    end
+    
+end
