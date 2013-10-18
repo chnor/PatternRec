c666d94a66e8c313a7c746a0b9566afeb053c286
Christopher Norman <chnor@kth.se>
2013-10-18 12:35:40 +0200

    InpretWord Hacks

--- Assignment5/InterpretWord.m
+++ Assignment5/InterpretWord.m
@@ -12,11 +12,20 @@ function word = InterpretWord(Classifier, data)
     features = cellfun(@PreprocessData, data, 'UniformOutput', false);
     responses = zeros(length(Classifier.HMMs), length(data));
     for i = 1:size(data, 2)
+%         InterpretChar(Classifier, data{i})
         responses(:, i) = logprob([Classifier.HMMs{1, :}], features{i});
         responses(:, i) = log(Classifier.priors)' + responses(:, i);
+%         responses(:, i) = - cell2mat(Classifier.thetas)' + responses(:, i);
     end
     
+    % Hack
+%     responses(1, :) = responses(1, :) + 75;
+%     responses(3, :) = responses(3, :) + 150;
+%     responses(12, :) = responses(12, :) + 50;
+%     responses(20, :) = responses(20, :) - 50;
+    
     [~, decision] = max(responses, [], 1);
+%     [~, decision] = sort(responses, 'descend');
     word = Classifier.classes(decision);
     
 end
